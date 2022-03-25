873721aa457dca8c3f9c3668a7a71eb860b8bf85
Rafaela Brandão Mania Lima <60527114+Rafaela-create013@users.noreply.github.com>
2022-03-25 16:16:58 -0300

    atividades 1 e 2 de Flutter

---
+++ app_contador/lib/home.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String infoResultado = "0";
+  int contador = 0;
+
+  void _calcularSoma() {
+    setState(() {
+      contador = contador + 1;
+      infoResultado = contador.toString();
+    });
+  }
+
+  void _calcularSubtracao() {
+    setState(() {
+      contador = contador - 1;
+      infoResultado = contador.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _titulo(),
+      backgroundColor: Colors.white,
+      body: _corpo(),
+    );
+  }
+
+  _titulo() {
+    return AppBar(
+      title: Text("Contador de pessoas"),
+      centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 2, 59, 55),
+    );
+  }
+
+  _corpo() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _texto(infoResultado),
+          _botaoSoma(),
+          _botaoDiminui(),
+        ],
+      ),
+    );
+  }
+
+  _botaoSoma() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _calcularSoma,
+          child:
+              Text("+", style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Color.fromARGB(255, 2, 59, 55),
+        ),
+      ),
+    );
+  }
+
+  _botaoDiminui() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _calcularSubtracao,
+          child:
+              Text("-", style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Color.fromARGB(255, 2, 59, 55),
+        ),
+      ),
+    );
+  }
+
+  _texto(textoParaExibir) {
+    return Text(
+      textoParaExibir,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 25.0),
+    );
+  }
+}
